3daba6b8a80fbd2bdaea24aded11cd239a336108
pbmacintyre <pbmacintyre@gmail.com>
2021-03-09 11:43:11 -0400

    Add files via upload

---
+++ opt-outs-SMS.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("ringcentral-functions.inc");
+
+$db = db_connect();
+
+$sdk = ringcentral_invoke_sdk() ;
+if (is_object($sdk )) {
+    echo "SDK Connected... <br/>";
+} else {
+    echo "<pre>" ;
+    var_dump($sdk) ;
+    echo "</pre>" ;
+    exit();
+}
+
+/* now send a simple SMS message */
+/* get the phone number from which the message will be sent */
+    
+$params = array( 'from' => ringcentral_get_from_phone() );
+
+try {
+//     $resp = $sdk->platform()->get('/account/~/extension/~/sms/opt-outs', $params);    
+    $resp = $sdk->platform()->get('/restapi/v1.0/account/~/a2p-sms/opt-outs', $params);    
+} catch (\RingCentral\SDK\Http\ApiException $e) {    
+    $apiResponse = $e->apiResponse();
+    // craft a friendly message here.    
+    echo "<h2>There was an error communicating with the RingCentral servers </h2>";    
+    echo "<pre>" ;
+    var_dump($apiResponse) ;
+    echo "</pre>" ;
+}
+
+echo "<pre>" ;
+var_dump($resp->json());
+echo "</pre>" ;
+
+?>
