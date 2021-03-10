51f6d151d87ca8e53a9e2ee30ede46b67fe1e172
Peter MacIntyre <pbmacintyre@gmail.com>
2021-03-10 13:54:24 -0400

    Added testing of A2P provisioning

---
+++ test-HVSMS-number.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (C) 2021 Paladin Business Solutions
+ */
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once("ringcentral-functions.inc");
+
+$db = db_connect();
+
+$sdk = ringcentral_invoke_sdk() ;
+if (is_object($sdk )) {
+    echo "SDK Connected... <br/>";
+} else {
+    echo "<pre>" ;
+    var_dump($sdk) ;
+    echo "</pre>" ;
+    exit();
+}
+
+/* now send a request to check the numbers */
+$resp = $sdk->platform()->get('/account/~/extension/~/phone-number');
+foreach ($resp->json()->records as $record){
+    foreach ($record->features as $feature){
+        if ($feature == "A2PSmsSender"){
+            if ($record->paymentType == "TollFree")
+                print_r ("This phone number ".$record->phoneNumber." is a toll-free number and provisioned for using to send high volume SMS\n");
+            else
+                print_r ("This phone number ".$record->phoneNumber." is a 10-DLC local number and provisioned for using to send high volume SMS\n");
+        }
+    }
+}
+?>
